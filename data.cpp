omnirom/android_bootable_recovery

commit eeed3c55d82fc19e34fd78a188b4a5367923901b

Fix handling of storage with data/media vs data/media/0
Sometimes loading the values from the settings file may set the
storage path to /data/media when the proper path is /data/media/0
This commit will grab the storage path from the partition manager
to ensure that we are using the proper path.

Change-Id: Ibfd63125a2b9705319eb55b09c7e65391757bbc2

data.cpp
@@ -279,9 +279,10 @@ int DataManager::LoadValues(const string filename)
 error:
 	fclose(in);
 	string current = GetCurrentStoragePath();
-	string settings = GetSettingsStoragePath();
-	if (current != settings && !PartitionManager.Mount_By_Path(current, false)) {
-		SetValue("tw_storage_path", settings);
+	TWPartition* Part = PartitionManager.Find_Partition_By_Path(current);
+	if (current != Part->Storage_Path && Part->Mount(false)) {
+		LOGINFO("LoadValues setting storage path to '%s'\n", Part->Storage_Path.c_str());
+		SetValue("tw_storage_path", Part->Storage_Path);
 	} else {
 		SetBackupFolder();
 	}

JDCTeam/TWRP

commit 6db64daba3c7e984c81f10b2e9c8833e03237db3

Fix crash while loading config introduced by eeed3c55d82fc19e34fd78a188b4a5367923901b
Change-Id: Ia5957efe616defa4272501cd8f31d26d394077bb

data.cpp
@@ -280,7 +280,9 @@ error:
 	fclose(in);
 	string current = GetCurrentStoragePath();
 	TWPartition* Part = PartitionManager.Find_Partition_By_Path(current);
-	if (current != Part->Storage_Path && Part->Mount(false)) {
+	if(!Part)
+		Part = PartitionManager.Get_First_Storage_Partition();
+	if (Part && current != Part->Storage_Path && Part->Mount(false)) {
 		LOGINFO("LoadValues setting storage path to '%s'\n", Part->Storage_Path.c_str());
 		SetValue("tw_storage_path", Part->Storage_Path);
 	} else {
@@ -586,10 +588,15 @@ void DataManager::SetDefaultValues()
 	mConstValues.insert(make_pair("false", "0"));
 
 	mConstValues.insert(make_pair(TW_VERSION_VAR, TW_VERSION_STR));
-	mValues.insert(make_pair("tw_storage_path", make_pair("/", 1)));
 	mValues.insert(make_pair("tw_button_vibrate", make_pair("80", 1)));
 	mValues.insert(make_pair("tw_keyboard_vibrate", make_pair("40", 1)));
 	mValues.insert(make_pair("tw_action_vibrate", make_pair("160", 1)));
+
+	TWPartition *store = PartitionManager.Get_First_Storage_Partition();
+	if(store)
+		mValues.insert(make_pair("tw_storage_path", make_pair(store->Storage_Path.c_str(), 1)));
+	else
+		mValues.insert(make_pair("tw_storage_path", make_pair("/", 1)));
 
 #ifdef TW_FORCE_CPUINFO_FOR_DEVICE_ID
 	printf("TW_FORCE_CPUINFO_FOR_DEVICE_ID := true\n");

data.hpp
@@ -85,7 +85,6 @@ protected:
 private:
 	static void sanitize_device_id(char* device_id);
 	static void get_device_id(void);
-
 };
 
 #endif // _DATAMANAGER_HPP_HEADER

partitionmanager.cpp
@@ -1980,6 +1980,15 @@ void TWPartitionManager::Output_Storage_Fstab(void) {
 	fclose(fp);
 }
 
+TWPartition *TWPartitionManager::Get_First_Storage_Partition()
+{
+	for (std::vector<TWPartition*>::iterator iter = Partitions.begin(); iter != Partitions.end(); ++iter) {
+		if((*iter)->Is_Storage)
+			return *iter;
+	}
+	return NULL;
+}
+
 bool TWPartitionManager::Push_Context()
 {
 	sync();

partitions.hpp
@@ -217,6 +217,7 @@ public:
 	void Mount_All_Storage(void);                                             // Mounts all storage locations
 	void UnMount_Main_Partitions(void);                                       // Unmounts system and data if not data/media and boot if boot is mountable
 	int Partition_SDCard(void);                                               // Repartitions the sdcard
+	TWPartition *Get_First_Storage_Partition();                               // Returns a pointer to first partition with Is_Storage
 
 	int Fix_Permissions();
 	void Get_Partition_List(string ListType, std::vector<PartitionList> *Partition_List);