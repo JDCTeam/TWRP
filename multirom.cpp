d7fd974b8b8d9e508bec2a307f68fb4535cd3015
Mahmood(Thunder07) <m.a.hot.m+git@gmail.com>
2014-05-26 10:56:54 +0100

    fixed mount check & zip updating with spaces

--- multirom.cpp
+++ multirom.cpp
@@ -908,6 +908,11 @@ static char *strstr_wildcard(const char *s, const char *find)
 
 bool MultiROM::skipLine(const char *line)
 {
+
+	if(strstr(line, "mount") && strstr(line, "ui_print"))
+		if (strstr(line, "mount") < strstr(line, "ui_print"))
+			return true;
+
 	if(strstr(line, "mount") && !strstr(line, "ui_print"))
 	{
 		if (strstr(line, "run_program") ||
@@ -926,7 +931,7 @@ bool MultiROM::skipLine(const char *line)
 		return false;
 	}
 
-	if(strstr(line, "/dev/block/platform/"))
+	if(strstr(line, "/dev/block/"))
 		return true;
 
 	if(strstr(line, "\"dd\"") && strstr(line, "run_program"))
@@ -1034,7 +1039,7 @@ bool MultiROM::prepareZIP(std::string& file)
 
 	if(changed)
 	{
-		sprintf(cmd, "cd /tmp && zip %s %s", file.c_str(), MR_UPDATE_SCRIPT_NAME);
+		sprintf(cmd, "cd /tmp && zip \"%s\" %s", file.c_str(), MR_UPDATE_SCRIPT_NAME);
 		if(system(cmd) < 0)
 			return false;
 	}
@@ -2353,7 +2358,7 @@ bool MultiROM::fakeBootPartition(const char *fakeImg)
 	}
 
 	system_args("echo '%s' > /tmp/mrom_fakebootpart", m_boot_dev.c_str());
-	system_args("mv \"%s\" \"%s\"-orig", m_boot_dev.c_str(), m_boot_dev.c_str());
+	system_args("mv \"%s\" \"%s-orig\"", m_boot_dev.c_str(), m_boot_dev.c_str());
 	system_args("ln -s \"%s\" \"%s\"", fakeImg, m_boot_dev.c_str());
 
 #ifdef BOARD_BOOTIMAGE_PARTITION_SIZE
