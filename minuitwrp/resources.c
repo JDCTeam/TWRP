6f503fddefe2d8d1aab588c194d13b2a90be8caa
Vojtech Bocek <vbocek@gmail.com>
2015-01-12 19:03:49 +0100

    Fix PNG loading in landscape mode
    Change-Id: Iff7b30a4619f1dd60f1add156fa4615571bd97d0

--- minuitwrp/resources.c
+++ minuitwrp/resources.c
@@ -59,26 +59,22 @@ static int open_png(const char* name, png_structp* png_ptr, png_infop* info_ptr,
     unsigned char header[8];
     int result = 0;
 
-    snprintf(resPath, sizeof(resPath), "/res/images/%s.png", name);
+#ifdef TW_HAS_LANDSCAPE
+    if(gr_get_rotation()%180 != 0)
+        snprintf(resPath, sizeof(resPath), "/res/landscape/images/%s.png", name);
+    else
+#endif
+    {
+        snprintf(resPath, sizeof(resPath), "/res/images/%s.png", name);
+    }
+
     FILE* fp = fopen(resPath, "rb");
     if (fp == NULL) {
-#ifndef TW_HAS_LANDSCAPE
         fp = fopen(name, "rb");
         if (fp == NULL) {
             result = -1;
             goto exit;
         }
-#else
-        if(gr_get_rotation()%180 == 0)
-            snprintf(resPath, sizeof(resPath), "%s", name);
-        else
-            snprintf(resPath, sizeof(resPath), "/res/landscape/images/%s.png", name);
-        fp = fopen(resPath, "rb");
-        if (fp == NULL && (gr_get_rotation()%180 == 0 || !(fp = fopen(name, "rb")))) {
-            result = -1;
-            goto exit;
-        }
-#endif
     }
 
     size_t bytesRead = fread(header, 1, sizeof(header), fp);
