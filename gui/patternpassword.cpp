ab0a993a1e57fd49473c27c43fc055207a75ca4f
Vojtech Bocek <vbocek@gmail.com>
2015-03-10 11:34:36 +0100

    Add vibration to GUIPatternPassword
    Change-Id: Ib79d92ba4ce6a889f48a00d940774a723465e9c9

--- gui/patternpassword.cpp
+++ gui/patternpassword.cpp
@@ -257,6 +257,7 @@ int GUIPatternPassword::NotifyTouch(TOUCH_STATE state, int x, int y)
 			mTrackingTouch = true;
 			ResetActiveDots();
 			ConnectDot(dot_idx);
+			DataManager::Vibrate("tw_button_vibrate");
 			mCurLineX = x;
 			mCurLineY = y;
 			mNeedRender = true;
@@ -272,6 +273,7 @@ int GUIPatternPassword::NotifyTouch(TOUCH_STATE state, int x, int y)
 			{
 				ConnectIntermediateDots(dot_idx);
 				ConnectDot(dot_idx);
+				DataManager::Vibrate("tw_button_vibrate");
 			}
 
 			mCurLineX = x;
